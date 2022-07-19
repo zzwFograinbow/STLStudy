fc99f9ebc79386fd13f4b1615f356c4f500a949a
zzwFograinbow <zzwfog@163.com>
2022-07-19 18:21:56 +0800

    new

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <string>
 
+void printVector(std::vector<int>&v);
 void show(int val)
 {
     std::cout<<val<<std::endl;
@@ -62,5 +63,53 @@ int main()
         std::cout<<(*it).m_name<<" "<<(*it).m_age<<std::endl;
         std::cout<<it->m_name<<"  "<<it->m_age<<std::endl;
     }
+    /*vector的构造方式*/
+
+    std::vector<int> v1;                               //vector的无参调用
+    for(int i = 0;i < 10;i++)
+    {
+        v1.push_back(i);
+    }
+    printVector(v1);
+
+    std::vector<int> v2(v1.begin(),v1.end());//区间方式进行构造
+    printVector(v2);
+
+    //std::cout<<*(v1.begin());
+
+    std::vector<int> v3(10,100);             //n个elem的方式进行构造,前面的是个数，后面的是值
+    printVector(v3);
+
+    std::vector<int> v4(v3);                         //复制构造
+    printVector(v4);
+
+    /*vector的赋值操作*/
+    //1、operator=
+    std::vector<int>v11 = v1;
+    printVector(v11);
+
+    //2、assign 区间赋值
+    std::vector<int> v22;
+    v22.assign(v11.begin(),v11.end());
+    printVector(v22);
+
+    //3、assign n个elem赋值
+    std::vector<int> v33;
+    v33.assign(10,100);
+    printVector(v33);
+
+    /*vector 容器的大小和容量*/
+    std::cout<<v1.empty()<<std::endl;
+    std::cout<<v1.capacity()<<std::endl;
+    std::cout<<v1.size()<<std::endl;
     return 0;
+}
+
+void printVector(std::vector<int>&v)
+{
+    for(std::vector<int>::iterator it = v.begin();it!=v.end();it++)
+    {
+        std::cout<<*it<<" ";//std::endl;
+    }
+    std::cout<<std::endl;
 }
